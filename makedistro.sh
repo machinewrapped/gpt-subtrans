3e289f3f5c44542f2981cf07c0e3224f9f424a16
machinewrapped <machinewrapped@users.noreply.github.com>
2023-05-23 21:29:59 +0200

    Updated instructions
    And make sure to include them all in bundled releases.

--- makedistro.sh
+++ makedistro.sh
@@ -2,4 +2,4 @@
 
 source envsubtrans/bin/activate
 pip install -r requirements.txt
-pyinstaller --add-data "theme/*:theme/" --add-data "instructions.txt:." --add-data "LICENSE:." gui-subtrans.py
+pyinstaller --add-data "theme/*:theme/" --add-data "instructions*:." --add-data "LICENSE:." gui-subtrans.py
