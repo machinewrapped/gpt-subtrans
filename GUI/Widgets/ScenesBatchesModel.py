d8cf59fec382d6df884fa761aa332851827feea7
machinewrapped <machinewrapped@users.noreply.github.com>
2023-06-23 05:43:10 +0200

    Use a simpler proxy model for SceneBatches

--- GUI/Widgets/ScenesBatchesModel.py
+++ GUI/Widgets/ScenesBatchesModel.py
@@ -1,42 +1,37 @@
 import logging
-from PySide6.QtCore import QAbstractItemModel, QModelIndex, Qt
+from PySide6.QtCore import QSortFilterProxyModel, QModelIndex, Qt
 
 from GUI.ProjectViewModel import BatchItem, ViewModelItem
 from GUI.Widgets.Widgets import TreeViewItemWidget
 
-class ScenesBatchesModel(QAbstractItemModel):
+class ScenesBatchesModel(QSortFilterProxyModel):
     def __init__(self, viewmodel=None, parent=None):
         super().__init__(parent)
-        self.viewmodel = viewmodel
-        self.root_item = self.viewmodel.getRootItem() if self.viewmodel else None
+        if viewmodel:
+            self.setSourceModel(viewmodel)
 
-    def rowCount(self, parent=QModelIndex()):
-        if not parent.isValid():
-            return self.root_item.rowCount() if self.root_item else 0
+    def filterAcceptsRow(self, source_row : int, source_parent : QModelIndex):
+        if not self.sourceModel():
+            return False
         
-        parent_item = parent.internalPointer()
+        if not source_parent.isValid():
+            return True
 
-        # Nothing below Batch level
-        if isinstance(parent_item, BatchItem):
-            return 0
-        
-        return parent_item.rowCount()
+        item = self.sourceModel().itemFromIndex(source_parent)
 
-    def columnCount(self, parent=QModelIndex()):
-        return 1
+        if not item or isinstance(item, BatchItem):
+            return False
 
-    def itemFromIndex(self, index):
-        if not index.isValid():
-            return None
-
-        item = index.internalPointer()
-        return item
+        return True
 
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
 
-        item : ViewModelItem = index.internalPointer()
+        source_index = self.mapToSource(index)
+        item : ViewModelItem = self.sourceModel().itemFromIndex(source_index)
+        if not isinstance(item, ViewModelItem):
+            return None
 
         if role == Qt.ItemDataRole.UserRole:
             return item
@@ -48,30 +43,3 @@ class ScenesBatchesModel(QAbstractItemModel):
             return TreeViewItemWidget(item.GetContent()).sizeHint()
 
         return None
-
-    def index(self, row, column, parent=QModelIndex()):
-        if not self.hasIndex(row, column, parent):
-            return QModelIndex()
-
-        if not parent.isValid():
-            child_item = self.root_item.child(row)
-        else:
-            parent_item = parent.internalPointer()
-            child_item = parent_item.child(row)
-
-        if child_item:
-            return self.createIndex(row, column, child_item)
-        else:
-            return QModelIndex()
-
-    def parent(self, index):
-        if not index.isValid():
-            return QModelIndex()
-
-        child_item = index.internalPointer()
-        parent_item = child_item.parent()
-
-        if parent_item == self.root_item or parent_item is None:
-            return QModelIndex()
-
-        return self.createIndex(parent_item.row(), 0, parent_item)
