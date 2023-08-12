7211825f1b6900489a04023b6abc296bacab1e96
machinewrapped <machinewrapped@users.noreply.github.com>
2023-08-12 03:49:16 +0200

    Try force installing universal2 libs
    This is experimental, pyinstaller reports errors with pyside6 but it still seems to run on an M2 Mac - to be seen if it works on Intel too.

--- makedistro-mac.sh
+++ makedistro-mac.sh
@@ -2,10 +2,5 @@
 
 source envsubtrans/bin/activate
 pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib pyinstaller
-pip install --upgrade -r requirements.txt
-pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib regex
-pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib srt
-pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib charset_normalizer
-pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib multidict
-pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib aiohttp
+pip install --platform=universal2 --no-deps --upgrade --force-reinstall --target ./envsubtrans/lib -r requirements.txt
 pyinstaller --paths="./envsubtrans/lib" --add-data "theme/*:theme/" --add-data "instructions*:." --add-data "LICENSE:." --target-arch universal2 --noconfirm gui-subtrans.py
