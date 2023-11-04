02ff59bd38eecfc930ae3a474adaf8bae7209baf
machinewrapped <machinewrapped@users.noreply.github.com>
2023-11-04 12:28:53 +0100

    Revert "Ignore OpenAI instruct models"
    This reverts commit b7fe86db92b73e28b301a43afa1291bfca71a2fe.

--- PySubtitle/SubtitleTranslator.py
+++ PySubtitle/SubtitleTranslator.py
@@ -67,10 +67,8 @@ class SubtitleTranslator:
             if not response or not response.data:
                 return []
 
-            model_list = [model.openai_id for model in response.data if model.openai_id.startswith('gpt') and model.openai_id.find('instruct') < 0]
-
+            model_list = [ model.openai_id for model in response.data if model.openai_id.startswith('gpt') ]
             return sorted(model_list)
-
         except Exception as e:
             logging.error(f"Unable to retrieve available AI models: {str(e)}")
             return []
