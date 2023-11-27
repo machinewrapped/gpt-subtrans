3497a0897b9991653f6b9d752e2920bae4e73334
machinewrapped <machinewrapped@users.noreply.github.com>
2023-11-27 22:22:09 +0100

    Attempts to get the Mac distribution working
    So far the only luck I've had is to create a non-universal package.

--- makedistro-mac.sh
+++ makedistro-mac.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source envsubtrans/bin/activate
+source ./envsubtrans/bin/activate
 pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib pyinstaller
-pip install --platform=universal2 --no-deps --upgrade --force-reinstall --target ./envsubtrans/lib -r requirements.txt
-pyinstaller --paths="./envsubtrans/lib" --add-data "theme/*:theme/" --add-data "instructions*:." --add-data "LICENSE:." --add-data "gui-subtrans.ico:." --target-arch universal2 --noconfirm gui-subtrans.py
+pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib -r requirements.txt
+pip install --platform=universal2 --no-deps --upgrade --target ./envsubtrans/lib charset_normalizer
+pip install --upgrade --target ./envsubtrans/lib pydantic_core
+pip install --upgrade --target ./envsubtrans/lib pyside6
+#pyinstaller --paths="./envsubtrans/lib" --paths="./envsubtrans/lib/python3.12/site-packages"  --add-data "theme/*:theme/" --add-data "instructions*:." --add-data "LICENSE:." --add-data "gui-subtrans.ico:." --target-arch universal2 --noconfirm gui-subtrans.py
+pyinstaller --paths="./envsubtrans/lib" --paths="./envsubtrans/lib/python3.12/site-packages" --add-data "theme/*:theme/" --add-data "instructions*:." --add-data "LICENSE:." --add-data "gui-subtrans.ico:." --noconfirm gui-subtrans.py
