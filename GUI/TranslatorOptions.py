ee143be55399019a490c8d0792abb3a555ab88e3
machinewrapped <machinewrapped@users.noreply.github.com>
2023-12-23 15:28:54 +0100

    Added prompt to instructions file
    Split instruction handling out of Options into a helper class with load and save.
    Renamed GPT options to Translator options, might as well start moving towards a generic system.

--- GUI/TranslatorOptions.py
+++ GUI/TranslatorOptions.py
@@ -9,9 +9,11 @@ from PySide6.QtWidgets import (
     QPushButton, 
     QFileDialog, 
     )
+from GUI.GuiHelpers import GetResourcePath
 from GUI.Widgets.OptionsWidgets import MULTILINE_OPTION, CreateOptionWidget
 
 from PySubtitle.Options import Options
+from PySubtitle.Instructions import Instructions
 from PySubtitle.SubtitleTranslator import SubtitleTranslator
 
 class TranslatorOptionsDialog(QDialog):
@@ -20,16 +22,16 @@ class TranslatorOptionsDialog(QDialog):
         self.setWindowTitle("Translator Options")
         self.setMinimumWidth(800)
 
-        self.data = data
+        self.model = data.get('model', '')
         self.models = SubtitleTranslator.GetAvailableModels(data.get('api_key'), data.get('api_base'))
 
-        self.source_prompt = self.data.get('gpt_prompt')
-        self.source_instructions = self.data.get('instructions')
+        self.instructions : Instructions = Instructions(data)
 
         self.form_layout = QFormLayout()
-        self.model_edit = self._add_form_option("model", self.data.get('gpt_model', ''), self.models, tooltip="Model to use for translation")
-        self.prompt_edit = self._add_form_option("prompt", self.data.get('gpt_prompt', ''), str, "Prompt for each user translation request")
-        self.instructions_edit = self._add_form_option("instructions", self.data.get('instructions', ''), MULTILINE_OPTION, "System instructions for the translator")
+        self.model_edit = self._add_form_option("model", self.model, self.models, tooltip="Model to use for translation")
+        self.prompt_edit = self._add_form_option("prompt", self.instructions.prompt, str, "Prompt for each translation request")
+        self.instructions_edit = self._add_form_option("instructions", self.instructions.instructions, MULTILINE_OPTION, "System instructions for the translator")
+        self.retry_instructions_edit = self._add_form_option("retry_instructions", self.instructions.retry_instructions, MULTILINE_OPTION, "Supplementary instructions when retrying")
 
         self.button_layout = QHBoxLayout()
 
@@ -57,11 +59,13 @@ class TranslatorOptionsDialog(QDialog):
         return button
 
     def accept(self):
-        self.data['gpt_model'] = self.model_edit.GetValue()
-        self.data['gpt_prompt'] = self.prompt_edit.GetValue()
-        self.data['instructions'] = self.instructions_edit.GetValue()
+        self.model = self.model_edit.GetValue()
 
-        self._check_for_edited_instructions()
+        if self._check_for_edited_instructions():
+            self.instructions.prompt = self.prompt_edit.GetValue()
+            self.instructions.instructions = self.instructions_edit.GetValue()
+            self.instructions.retry_instructions = self.retry_instructions_edit.GetValue()
+            self.instructions.instruction_file = None
 
         super().accept()
 
@@ -69,11 +73,13 @@ class TranslatorOptionsDialog(QDialog):
         super().reject()
 
     def _check_for_edited_instructions(self):
-        if self.data['instructions'] != self.source_instructions:
-            self.data['instruction_file'] = ""
-            self.data['instructions_edited'] = True
-        if self.data['gpt_prompt'] != self.source_prompt:
-            self.data['instructions_edited'] = True
+        '''Check if the instructions have been edited'''
+        if self.prompt_edit.GetValue() != self.instructions.prompt:
+            return True
+        if self.instructions_edit.GetValue() != self.instructions.instructions:
+            return True
+        if self.retry_instructions_edit.GetValue() != self.instructions.retry_instructions:
+            return True
 
     @property
     def load_icon(self):
@@ -84,40 +90,39 @@ class TranslatorOptionsDialog(QDialog):
         return QApplication.style().standardIcon(QStyle.StandardPixmap.SP_DialogSaveButton)
     
     def _load_instructions(self):
+        '''Load instructions from a file'''
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, "Load Instructions", "", "Text Files (*.txt);;All Files (*)", options=options)
         if file_name:
             try:
-                with open(file_name, "r", encoding='utf-8') as file:
-                    content = file.read()
-                    self.instructions_edit.SetValue(content)
+                self.instructions.LoadInstructionsFile(file_name)
 
-                self.source_instructions = content
-                self.data['instruction_file'] = file_name
+                self.prompt_edit.SetValue(self.instructions.prompt)
+                self.instructions_edit.SetValue(self.instructions.instructions)
+                self.retry_instructions_edit.SetValue(self.instructions.retry_instructions)
 
             except Exception as e:
                 logging.error(f"Unable to read instruction file: {str(e)}")
 
     def _save_instructions(self):
+        '''Save instructions to a file'''
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getSaveFileName(self, "Save Instructions", "", "Text Files (*.txt);;All Files (*)", options=options)
+        filepath = GetResourcePath(self.instructions.instruction_file)
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save Instructions", filepath, "Text Files (*.txt);;All Files (*)", options=options)
         if file_name:
-            content = self.instructions_edit.GetValue()
             try:
-                if not file_name.endswith('.txt'):
-                    file_name += '.txt'
-                with open(file_name, "w", encoding='utf-8') as file:
-                    file.write(content)
-
-                self.source_instructions = content
-                self.data['instruction_file'] = file_name
+                self.instructions.prompt = self.prompt_edit.GetValue()
+                self.instructions.instructions = self.instructions_edit.GetValue()
+                self.instructions.retry_instructions = self.retry_instructions_edit.GetValue()
+                
+                self.instructions.SaveInstructions(file_name)
 
             except Exception as e:
                 logging.error(f"Unable to write instruction file: {str(e)}")
 
     def set_defaults(self):
         options = Options()
-        self.model_edit.SetValue(options.get('gpt_model'))
-        self.prompt_edit.SetValue(options.get('gpt_prompt'))
+        self.model_edit.SetValue(options.get('model'))
+        self.prompt_edit.SetValue(options.get('prompt'))
         self.instructions_edit.SetValue(options.get('instructions'))
-        self.data['instruction_file'] = options.get('instruction_file')
+        self.retry_instructions_edit.SetValue(options.get('retry_instructions'))
---
+++ PySubtitle/Instructions.py
@@ -0,0 +1,120 @@
+import logging
+import os
+
+linesep = '\n'
+
+default_instructions = linesep.join([
+    "You are to translate subtitles to a target language",
+    "Be concise but try to make the dialogue sound natural.",
+    "Translations should be as accurate as possible, do not improvise!",
+])
+
+default_retry_instructions = linesep.join([
+    'Translate the subtitles again, making sure every line has a translation that matches the dialog.'
+])
+
+class Instructions:
+    def __init__(self, settings):
+        self.InitialiseInstructions(settings)
+
+    def InitialiseInstructions(self, settings : dict):
+        self.prompt = settings.get('prompt') or settings.get('gpt_prompt')
+        self.instructions = settings.get('instructions') or default_instructions
+        self.retry_instructions = settings.get('retry_instructions') or default_retry_instructions
+        self.instruction_file = settings.get('instruction_file') or None
+
+        # Add any additional instructions from the command line
+        if settings.get('instruction_args'):
+            additional_instructions = linesep.join(settings['instruction_args'])
+            if additional_instructions:
+                self.instructions = linesep.join([self.instructions, additional_instructions])
+
+        tags = {
+            "[ for movie]": f" for {settings.get('movie_name')}" if settings.get('movie_name') else "",
+            "[ to language]": f" to {settings.get('to_language')}" if settings.get('to_language') else "",
+        }
+
+        tags.update({ f"[{k}]": v for k, v in settings.items() if v })
+
+        self.prompt = ReplaceTags(self.prompt, tags)
+        self.instructions = ReplaceTags(self.instructions, tags)
+        self.retry_instructions = ReplaceTags(self.retry_instructions, tags)
+
+    def LoadInstructionsFile(self, filepath):
+        """
+        Try to load instructions from a text file.
+        """
+        if filepath and os.path.exists(filepath):
+            with open(filepath, "r", encoding="utf-8", newline='') as f:
+                lines = [l.strip() for l in f.readlines()]
+
+            if not lines:
+                return
+
+            if not lines[0].startswith('###'):
+                logging.info(f"Loading legacy instruction file: {filepath}")
+                file_instructions, file_retry_instructions = LoadLegacyInstructions(lines)
+                if file_instructions and file_retry_instructions:
+                    self.instructions = file_instructions
+                    self.retry_instructions = file_retry_instructions
+                return
+
+            sections = {}
+            for line in lines:
+                if line.startswith('###'):
+                    section_name = line[3:].strip()
+                    sections[section_name] = []
+                elif line.strip() or sections[section_name]:
+                    sections[section_name].append(line)
+
+            self.prompt = linesep.join(sections.get('prompt', []))
+            self.instructions = linesep.join(sections.get('instructions', []))
+            self.retry_instructions = linesep.join(sections.get('retry_instructions', [])) or default_retry_instructions
+            self.instruction_file = os.path.basename(filepath)
+
+            if not self.prompt or not self.instructions:
+                raise Exception("Invalid instruction file")
+
+    def SaveInstructions(self, filepath):
+        """
+        Save instructions to a text file.
+        """
+        if filepath:
+            # Make sure the file has a .txt extension
+            if not filepath.endswith('.txt'):
+                filepath += '.txt'
+
+            # Write each section to the file with a header
+            with open(filepath, "w", encoding="utf-8", newline='') as f:
+                f.write("### prompt\n")
+                f.write(self.prompt)
+                f.write("\n\n### instructions\n")
+                f.write(self.instructions)
+                f.write("\n\n### retry_instructions\n")
+                f.write(self.retry_instructions)
+                f.write("\n")
+
+            self.instruction_file = os.path.basename(filepath)
+
+def LoadLegacyInstructions(lines):
+    """
+    Retry instructions can be added to the file after a line of at least 3 # characters.
+    """
+    if lines:
+        for idx, item in enumerate(lines):
+            if len(item) >= 3 and all(c == '#' for c in item):
+                return linesep.join(lines[:idx]), linesep.join(lines[idx + 1:])
+
+        return linesep.join(lines), []
+        
+    return None, None
+
+def ReplaceTags(text, tags):
+    """
+    Replace option tags in a string with the value of the corresponding option.
+    """
+    if text:
+        for name, value in tags.items():
+            if value:
+                text = text.replace(f"[{name}]", str(value))
+    return text
