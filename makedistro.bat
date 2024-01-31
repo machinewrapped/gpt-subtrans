9745e8844e85b7e3d4478499b0fbf0e6d7e3a34e
machinewrapped <machinewrapped@users.noreply.github.com>
2024-01-31 21:57:23 +0100

    Automatically overwrite distribution

--- makedistro.bat
+++ makedistro.bat
@@ -1,3 +1,3 @@
 call envsubtrans/scripts/activate
 pip install --upgrade -r requirements.txt
-pyinstaller --add-data "theme/*;theme/" --add-data "instructions*;." --add-data "LICENSE;." --add-data "gui-subtrans.ico;." gui-subtrans.py
+pyinstaller --noconfirm --add-data "theme/*;theme/" --add-data "instructions*;." --add-data "LICENSE;." --add-data "gui-subtrans.ico;." gui-subtrans.py
