bb6c6cbafdcfc500ea4394f218e592e597df2a1a
machinewrapped <machinewrapped@users.noreply.github.com>
2024-01-31 20:49:37 +0100

    Fix parsing substitutions with blank lines

--- PySubtitle/Helpers.py
+++ PySubtitle/Helpers.py
@@ -271,7 +271,7 @@ def ParseSubstitutions(sub_list, separator="::"):
             if "::" in sub:
                 before, after = sub.split(separator)
                 substitutions[before] = after
-            elif sub.trim():
+            elif sub.strip():
                 try:
                     with open(sub, "r", encoding="utf-8", newline='') as f:
                         for line in [line.strip() for line in f if line.strip()]:
