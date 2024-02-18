bf4b30e0707847f7f1ae994f93868ebd0bb487fd
machinewrapped <machinewrapped@users.noreply.github.com>
2024-02-18 15:48:39 +0100

    Added model to command line args
    And removed some short-forms from arguments.

--- gpt-subtrans.py
+++ gpt-subtrans.py
@@ -19,18 +19,19 @@ parser = argparse.ArgumentParser(description='Translates an SRT file using GPT')
 parser.add_argument('input', help="Input SRT file path")
 parser.add_argument('-o', '--output', help="Output SRT file path")
 parser.add_argument('-l', '--target_language', type=str, default=None, help="The target language for the translation")
-parser.add_argument('-m', '--moviename', type=str, default=None, help="Optionally specify the name of the movie to help the translator")
+parser.add_argument('-m', '--model', type=str, default=None, help="The model to use for translation")
 parser.add_argument('-r', '--ratelimit', type=int, default=None, help="Maximum number of batches per minute to process")
 parser.add_argument('-k', '--apikey', type=str, default=None, help="Your OpenAI API Key (https://platform.openai.com/account/api-keys)")
 parser.add_argument('-t', '--temperature', type=float, default=0.0, help="A higher temperature increases the random variance of translations.")
 parser.add_argument('-p', '--project', type=str, default=None, help="Read or Write project file to working directory")
 parser.add_argument('-n', '--name', action='append', type=str, default=None, help="A name to use verbatim in the translation")
-parser.add_argument('-s', '--substitution', action='append', type=str, default=None, help="A pair of strings separated by ::, to subsitute in source or translation")
-parser.add_argument('-i', '--instruction', action='append', type=str, default=None, help="An instruction for Chat GPT about the translation")
-parser.add_argument('-f', '--instructionfile', type=str, default=None, help="Name/path of a file to load GPT instructions from")
 parser.add_argument('-b', '--apibase', type=str, default=None, help="API backend base address, the default value is https://api.openai.com/v1")
+parser.add_argument('--moviename', type=str, default=None, help="Optionally specify the name of the movie to help the translator")
 parser.add_argument('--description', type=str, default=None, help="A brief description of the film to give context")
 parser.add_argument('--names', type=str, default=None, help="A list of names to use verbatim")
+parser.add_argument('--substitution', action='append', type=str, default=None, help="A pair of strings separated by ::, to subsitute in source or translation")
+parser.add_argument('--instruction', action='append', type=str, default=None, help="An instruction for Chat GPT about the translation")
+parser.add_argument('--instructionfile', type=str, default=None, help="Name/path of a file to load GPT instructions from")
 parser.add_argument('--minbatchsize', type=int, default=None, help="Minimum number of lines to consider starting a new batch")
 parser.add_argument('--maxbatchsize', type=int, default=None, help="Maximum number of lines before starting a new batch is compulsory")
 parser.add_argument('--batchthreshold', type=float, default=None, help="Number of seconds between lines to consider for batching")
@@ -43,6 +44,7 @@ args = parser.parse_args()
 
 try:
     options = Options({
+        'model': args.model,
         'api_key': args.apikey,
         'api_base': args.apibase,
         'max_lines': args.maxlines,
