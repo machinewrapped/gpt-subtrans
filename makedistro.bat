dfdac17d9fa57761398ddffa33745b9b309bf892
machinewrapped <machinewrapped@users.noreply.github.com>
2024-03-02 07:43:00 +0100

    Ensure pyinstaller to make distro

--- makedistro.bat
+++ makedistro.bat
@@ -1,3 +1,5 @@
 call envsubtrans/scripts/activate
+pip install pywin32-ctypes
+pip install --upgrade pyinstaller
 pip install --upgrade -r requirements.txt
 pyinstaller --noconfirm --add-data "theme/*;theme/" --add-data "instructions*;." --add-data "LICENSE;." --add-data "gui-subtrans.ico;." gui-subtrans.py
