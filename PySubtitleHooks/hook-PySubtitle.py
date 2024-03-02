b582653e3358a927de6e9f3bade7e364c3cdb231
machinewrapped <machinewrapped@users.noreply.github.com>
2024-03-02 09:06:35 +0100

    PyInstaller hook

---
+++ PySubtitleHooks/hook-PySubtitle.py
@@ -0,0 +1,9 @@
+import logging
+
+try:
+    from PyInstaller.utils.hooks import collect_submodules
+
+    hiddenimports = collect_submodules('PySubtitle.Providers')
+
+except ImportError:
+    logging.info("PyInstaller not found, skipping hook")
--- makedistro.bat
+++ makedistro.bat
@@ -2,4 +2,4 @@ call envsubtrans/scripts/activate
 pip install pywin32-ctypes
 pip install --upgrade pyinstaller
 pip install --upgrade -r requirements.txt
-pyinstaller --noconfirm --add-data "theme/*;theme/" --add-data "instructions*;." --add-data "LICENSE;." --add-data "gui-subtrans.ico;." gui-subtrans.py
+pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --add-data "theme/*;theme/" --add-data "instructions*;." --add-data "LICENSE;." --add-data "gui-subtrans.ico;." gui-subtrans.py
