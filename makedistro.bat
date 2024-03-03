053072ce10c6859d7c893a8fbc92921aff6ebd0e
machinewrapped <machinewrapped@users.noreply.github.com>
2024-03-03 21:02:56 +0100

    Install/update providers in makesdistro

--- makedistro.bat
+++ makedistro.bat
@@ -3,4 +3,6 @@ python.exe -m pip install --upgrade pip
 pip install pywin32-ctypes
 pip install --upgrade pyinstaller
 pip install --upgrade -r requirements.txt
+pip install --upgrade openai
+pip install --upgrade google.generativeai
 pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --add-data "theme/*;theme/" --add-data "instructions*;." --add-data "LICENSE;." --add-data "gui-subtrans.ico;." gui-subtrans.py
