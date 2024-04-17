fd6ef70b08b4ef57cd25681462c9838a8851cc33
machinewrapped <machinewrapped@users.noreply.github.com>
2024-04-17 07:48:24 +0200

    Add assets folder to package

--- scripts/makedistro.sh
+++ scripts/makedistro.sh
@@ -2,4 +2,4 @@
 
 source envsubtrans/bin/activate
 pip install -r requirements.txt
-pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --add-data "theme/*:theme/" --add-data "instructions*:instructions/" --add-data "LICENSE:." --add-data "assets/gui-subtrans.ico:." scripts/gui-subtrans.py
+pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --add-data "theme/*:theme/"  --add-data "assets/*:assets/" --add-data "instructions*:instructions/" --add-data "LICENSE:." --add-data "assets/gui-subtrans.ico:." scripts/gui-subtrans.py
