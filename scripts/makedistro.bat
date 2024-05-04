2b5eacfca4438b5df4c6b64818db1f77319825bb
machinewrapped <machinewrapped@users.noreply.github.com>
2024-05-04 10:19:36 +0200

    Run unit tests in makedistro

--- scripts/makedistro.bat
+++ scripts/makedistro.bat
@@ -6,4 +6,11 @@ pip install --upgrade -r requirements.txt
 pip install --upgrade openai
 pip install --upgrade google.generativeai
 pip install --upgrade anthropic
+
+python.exe tests/unit_tests.py
+if %errorlevel% neq 0 (
+    echo Unit tests failed. Exiting...
+    exit /b %errorlevel%
+)
+
 pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --add-data "theme/*;theme/" --add-data "assets/*;assets/" --add-data "instructions*;instructions/" --add-data "LICENSE;." "scripts/gui-subtrans.py"
