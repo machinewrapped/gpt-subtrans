e2ea0e563808e76564141dd6e9d14f7a75a010f1
machinewrapped <machinewrapped@users.noreply.github.com>
2024-05-09 22:50:28 +0200

    Run unit tests for mac

--- scripts/makedistro-mac.sh
+++ scripts/makedistro-mac.sh
@@ -11,4 +11,10 @@ pip install --upgrade --target ./envsubtrans/lib anthropic
 pip install --upgrade --target ./envsubtrans/lib PySide6==6.6.0
 pip install --upgrade --target ./envsubtrans/lib PySide6.QtGui
 
+python tests/unit_tests.py
+if [ $? -ne 0 ]; then
+    echo "Unit tests failed. Exiting..."
+    exit $?
+fi
+
 pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --hidden-import="PySide6.QtGui" --paths="./envsubtrans/lib" --paths="./envsubtrans/lib/python3.12/site-packages" --add-data "theme/*:theme/" --add-data "assets/*:assets/"  --add-data "instructions*:instructions/" --add-data "LICENSE:." --noconfirm scripts/gui-subtrans.py
