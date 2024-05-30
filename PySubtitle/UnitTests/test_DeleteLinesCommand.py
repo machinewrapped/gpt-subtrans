8d385a1137025cff309e47504bb0fc4c5fd9f264
machinewrapped <machinewrapped@users.noreply.github.com>
2024-05-30 18:06:43 +0200

    More delete tests

--- PySubtitle/UnitTests/test_DeleteLinesCommand.py
+++ PySubtitle/UnitTests/test_DeleteLinesCommand.py
@@ -42,6 +42,18 @@ class DeleteLinesCommandTest(SubtitleTestCase):
             'lines_to_delete': [14],
             'expected_batch_size': 13,
             'expected_line_numbers': [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        },
+        {
+            'batch_number': (1,2),
+            'lines_to_delete': [15],
+            'expected_batch_size': 15,
+            'expected_line_numbers': [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+        },
+        {
+            'batch_number': (2,1),
+            'lines_to_delete': [33,34,35,36,37,38,39,40,41,42],
+            'expected_batch_size': 2,
+            'expected_line_numbers': [31,32]
         }
     ]
 
