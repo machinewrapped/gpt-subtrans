ae92a612a1a53aac0bf12886ec321b046df603a9
machinewrapped <machinewrapped@users.noreply.github.com>
2025-01-24 18:27:03 +0100

    Mistral script and installer
    (untested)

--- scripts/mistral-subtrans.py
+++ scripts/mistral-subtrans.py
@@ -12,22 +12,22 @@ from PySubtitle.Options import Options
 from PySubtitle.SubtitleProject import SubtitleProject
 from PySubtitle.SubtitleTranslator import SubtitleTranslator
 
-provider = "DeepSeek"
-default_model = os.getenv('DEEPSEEK_MODEL') or "deepseek-chat"
+provider = "Mistral"
+default_model = os.getenv('MISTRAL_MODEL') or "open-mistral-nemo"
 
-parser = CreateArgParser(f"Translates an SRT file using an DeepSeek model")
-parser.add_argument('-k', '--apikey', type=str, default=None, help=f"Your DeepSeek API Key (https://platform.deepseek.com/api_keys)")
-parser.add_argument('-b', '--apibase', type=str, default="https://api.deepseek.com", help="API backend base address.")
+parser = CreateArgParser(f"Translates an SRT file using an Mistral model")
+parser.add_argument('-k', '--apikey', type=str, default=None, help=f"Your Mistral API Key (https://console.mistral.ai/api-keys/)")
 parser.add_argument('-m', '--model', type=str, default=None, help="The model to use for translation")
+parser.add_argument('--server_url', type=str, default=None, help="Server URL (leave blank for default).")
 args = parser.parse_args()
 
-logger_options = InitLogger("deepseek-subtrans", args.debug)
+logger_options = InitLogger("mistral-subtrans", args.debug)
 
 try:
     options : Options = CreateOptions(
         args,
         provider,
-        api_base=args.apibase,
+        server_url=args.server_url,
         model=args.model or default_model
     )
 
