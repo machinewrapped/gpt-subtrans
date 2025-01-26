8ef080c2697ecba40611bacb0e1b3ad6a0308d12
machinewrapped <machinewrapped@users.noreply.github.com>
2025-01-26 10:00:57 +0100

    Makedistro dependency installation

--- scripts/makedistro.sh
+++ scripts/makedistro.sh
@@ -2,4 +2,9 @@
 
 source envsubtrans/bin/activate
 pip install -r requirements.txt
+pip install --upgrade openai
+pip install --upgrade google.generativeai
+pip install --upgrade anthropic
+pip install --upgrade mistralai
+
 pyinstaller --noconfirm --additional-hooks-dir="PySubtitleHooks" --add-data "theme/*:theme/"  --add-data "assets/*:assets/" --add-data "instructions*:instructions/" --add-data "LICENSE:." --add-data "assets/gui-subtrans.ico:." scripts/gui-subtrans.py
