2f9e39c284bd94f02ef75eb99ee2e07d854b4105
machinewrapped <machinewrapped@users.noreply.github.com>
2025-03-26 22:11:27 +0100

    Added google-api-core to install.bat
    Apparently we need to install it separately now, as on linux.

--- install-gemini.bat
+++ install-gemini.bat
@@ -6,7 +6,7 @@ call install.bat
 call envsubtrans\Scripts\activate.bat
 
 echo Installing Google Gemini SDK...
-pip install -U google-genai
+pip install -U google-genai google-api-core
 
 if not exist .env (
     REM Create .env file
